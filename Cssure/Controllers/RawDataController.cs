5e348b4ddc41b905083221af54ecc95024636b0e
EmmaSpanner <58326826+EmmaSpanner@users.noreply.github.com>
2023-04-03 00:31:58 +0200

    Added README and changed post method to receive byte[]

--- Cssure/Controllers/RawDataController.cs
+++ Cssure/Controllers/RawDataController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Net;
 using System.Text.Json;
+using System.Web.Http.Description;
 
 namespace Cssure.Controllers
 {
@@ -17,26 +18,39 @@ namespace Cssure.Controllers
 
         //Post method used to receive RawData from Patient app
         [HttpPost]
-        public async Task<ActionResult> Post([FromBody] RawData bytes)
+        public async Task<ActionResult> Post()
          {
 
-            //If the received data is null it returns bad request (Code 400) to patient app
-            if (bytes == null)
+            using (var stream = new MemoryStream())
             {
-                Debug.WriteLine("In RawDataController, failed to receive data");
+                await Request.Body.CopyToAsync(stream);
+                var bytes = stream.ToArray();
 
-                return BadRequest(new { message = "Failed to receive data." });
+                if (bytes == null)
+                {
+                    Debug.WriteLine("In RawDataController, failed to receive data");
+
+                    return BadRequest(new { msg = "Failed to receive data." }); // Sends a JSON object
+                    //return BadRequest("Failed to receive data.");
+                }
+                ////If the received data is null it returns bad request (Code 400) to patient app
+                //if (bytes == null)
+                //{
+                //    Debug.WriteLine("In RawDataController, failed to receive data");
+
+                //    return BadRequest(new { message = "Failed to receive data." });
+                //}
+
+                //RawData is send to backend for decoding and processing
+                await Task.Run(() => rawDataService.ProcessData(bytes));
+
+
+
+                Debug.WriteLine("In RawDataController, received data");
+
+                //If data is received succesfully the method returns an OK (Code 200) to patient App
+                return Ok(new { msg = "Data received" });
             }
-
-            //RawData is send to backend for decoding and processing
-            //_rawDataService.(bytes);
-
-
-
-            Debug.WriteLine("In RawDataController, received data");
-
-            //If data is received succesfully the method returns an OK (Code 200) to patient App
-            return Ok(new { message = "Data received" });
         }
     }
 }
