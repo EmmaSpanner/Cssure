43680205d880e3ba18d4ba06e61fd7863e1193f0
EmmaSpanner <58326826+EmmaSpanner@users.noreply.github.com>
2023-05-12 20:35:10 +0200

    Added methods for sending email in case of an alarm Part 2

---
+++ Cssure/AlarmSenders/EmailAlarmService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Cssure.AlarmSenders
+{
+    public class EmailAlarmService : IEmailSender
+    {
+        public Task SendEmailAsync(string[] email, string subject, string message)
+        {
+            var mail = "TemoTestMail@gmail.com";
+            var pw = "vkjrlfpkwkzhubaf";
+
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                UseDefaultCredentials = false,
+                EnableSsl = true,
+                Credentials = new NetworkCredential(mail, pw)
+            };
+
+            var mailMessage = new MailMessage(from: mail,
+                                to: email.First(), //First since we only type one email address
+                                subject,
+                                message);
+
+            mailMessage.Headers.Add("Importance", "High");
+            return client.SendMailAsync(mailMessage);
+        }
+    }
+}
