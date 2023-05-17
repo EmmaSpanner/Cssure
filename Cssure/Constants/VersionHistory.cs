9576406c951e459a16b88835f20ed46c3bc2f1ad
EmmaSpanner <58326826+EmmaSpanner@users.noreply.github.com>
2023-05-17 18:08:11 +0200

    Changed Version history

--- Cssure/Constants/VersionHistory.cs
+++ Cssure/Constants/VersionHistory.cs
@@ -7,7 +7,8 @@
         /// Hvilket version bliver der kørt
         /// </summary>
         //public static string No = "V1.1 - 2022-05-15 - Initial version"; 
-        public static string No = "V1.2 - 2023-05-15 - With Version control"; 
+        //public static string No = "V1.2 - 2023-05-15 - With Version control"; 
+        public static string No = "V1.3 - 2023-05-16 - Final version"; 
 
     }
 }
